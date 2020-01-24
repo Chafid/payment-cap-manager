87f05aa77b512eeb567ab589cda0bd844ee12e4f
Chafid Ahmad <chafid@gmail.com>
2020-01-24 17:42:39 +0700

    initial commit

---
+++ cap_input.php
@@ -0,0 +1,5 @@
+<form method="post" action="process.php">
+	Promo Name : <input type="text" name="promo_name" placeholder="Enter Promo Name" /><br />
+	Promo Cap : <input type="number" name="promo_cap" placeholder="Enter Promo Cap" /><br />
+<input type="submit" value="Submit" />
+</form>
